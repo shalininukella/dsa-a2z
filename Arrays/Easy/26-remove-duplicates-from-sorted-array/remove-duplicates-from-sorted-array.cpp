df0cc917303bc74cf9a57ac022b9438733353c38
Shalini Nukella <106904788+shalininukella@users.noreply.github.com>
2023-09-12 01:17:50 +0530

    Time: 8 ms (63.51%) | Memory: 18.6 MB (17.27%) - LeetSync

--- Arrays/Easy/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ Arrays/Easy/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,14 +1,28 @@
-class Solution {
-public:
-    int removeDuplicates(vector<int>& nums) {
-        set<int> a;
-        for( int i=0; i < nums.size(); i++){
-            a.insert(nums[i]);
-        }
-        auto it=a.begin();
-        for (int i =0; i < a.size(); i++, it++){
-            nums[i]= *it;
-        }
-        return a.size();
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//         set<int> a;
+//         for( int i=0; i < nums.size(); i++){
+//             a.insert(nums[i]);
+//         }
+//         auto it=a.begin();
+//         for (int i =0; i < a.size(); i++, it++){
+//             nums[i]= *it;
+//         }
+//         return a.size();
+//     }
+// };
+ class Solution {
+ public:
+int removeDuplicates(vector<int>& arr)
+{
+  int i = 0;
+  for (int j = 1; j < arr.size(); j++) {
+    if (arr[i] != arr[j]) {
+      i++;
+      arr[i] = arr[j];
     }
-};
+  }
+  return i + 1;
+}
+ };
