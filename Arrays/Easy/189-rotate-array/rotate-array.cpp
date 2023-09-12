2db6b3cade82f687aa2868ba2b9b536ff90d835d
Shalini Nukella <106904788+shalininukella@users.noreply.github.com>
2023-09-12 21:24:25 +0530

    Time: 24 ms (53.94%) | Memory: 24.9 MB (66.66%) - LeetSync

--- Arrays/Easy/189-rotate-array/rotate-array.cpp
+++ Arrays/Easy/189-rotate-array/rotate-array.cpp
@@ -1,15 +1,48 @@
+//using temp array 
+
+/*Time Complexity: O(n)
+
+Space Complexity: O(k) since k array element needs to be stored in temp array
+
+
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        k = k % n; // Ensure k is within bounds
 
-        if (k == 0) {
+        if (k == 0 || k>n) {
             return; // No need to rotate if k is 0
         }
+        k = k % n; // Ensure k is within bounds
 
         vector<int> temp(nums.begin() + n - k, nums.end()); // Store the last k elements in temp
         nums.erase(nums.begin() + n - k, nums.end()); // Remove the last k elements from nums
         nums.insert(nums.begin(), temp.begin(), temp.end()); // Insert the elements from temp at the beginning of nums
     }
+};
+
+*/
+
+//using reversal algorithm 
+class Solution {
+    public :
+    void reverse(vector<int>& a , int start, int end){
+        while(start<=end){
+            int temp = a[start];
+            a[start]= a[end];
+            a[end]=temp;
+            end--;
+            start++;
+        }
+    }
+     void rotate(vector<int>& a, int k ){
+         int n = a.size();
+         if(k==0){
+             return;
+         }
+         k=k%n;
+         reverse(a, 0, n-k-1);
+         reverse(a, n-k, n-1);
+         reverse(a, 0, n-1);
+     }
 };
