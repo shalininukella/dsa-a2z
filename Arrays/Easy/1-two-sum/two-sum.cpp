1e90bd2b0bcea0b0d91792a251e0dc854366db72
Shalini Nukella <106904788+shalininukella@users.noreply.github.com>
2023-09-13 22:40:32 +0530

    Time: 4 ms (95.48%) | Memory: 11.2 MB (13.84%) - LeetSync

--- Arrays/Easy/1-two-sum/two-sum.cpp
+++ Arrays/Easy/1-two-sum/two-sum.cpp
@@ -1,21 +1,49 @@
+/*//optimised using two pointers(greedy way) when yes/no is supposed to be returned.
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-       
-        //We will store our values and the indices here.
-        unordered_map<int,int> map;        
-		
-        //We iterate through the loop.
-        for(int index = 0; index < nums.size(); index++){
-			if(map.find(target-nums[index]) != map.end()){
-                //If map contains the value target - nums[index] return the indices of these two values.
-                return {index, map[target-nums[index]]};
-            }			
-            map[nums[index]] = index;
-            //If nota dd nums[index] to map.
-        }
-        
-        //If there is no solution return an empty vector.        
-        return {};
+    vector<int> twoSum(vector<int>& a, int target) {
+       int n = a.size();
+       int i=0, j=n-1;
+       sort(a.begin(), a.end()); //o(nlogn)
+       while (i<=j)//o(n)
+       {
+           int sum=a[i]+a[j];
+           if(sum<target)// need a greater no.
+           {
+               i++;
+           }
+           else if(sum> target)// need a smaller no.
+           {
+                j--;
+           }
+           else{
+               break; //given has atleast one solution, so once the loop is broken obviously we'll return the indices of the sum only.
+           }
+       }
+        return "YES";
     }   
+};*/
+
+
+//using hashmap i.e unordered map. optimised here
+/*define a hashmap with the elements of arrays as the key values of map and elemetns of map as the indices so that it becomes easier to access the index of a required element, i.e target-x, where x is is the current element*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& a, int target) {
+       int n = a.size();
+       unordered_map<int, int> mp;
+       //mp.find(key), if key not found find will return mp.end();
+       for(int i=0; i<n; i++){
+           int num = a[i];
+           int remain = target-a[i];
+           if(mp.find(remain)!=mp.end()){
+               return {mp[remain], i};
+           }       
+           /*inserting num now cus we dont want to compare the num with num, hence compare num with other nos. in map and then insert num in the map. */
+             mp[num]=i;
+           }
+           //it won't get executed at all since in this que each input has an ans
+           return {-1, -1};
+    }
 };
