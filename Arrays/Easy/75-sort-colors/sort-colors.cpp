5e84a1202a7026425d076fc8898a76ef528b342b
Shalini Nukella <106904788+shalininukella@users.noreply.github.com>
2023-09-14 16:00:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (13.65%) - LeetSync

--- Arrays/Easy/75-sort-colors/sort-colors.cpp
+++ Arrays/Easy/75-sort-colors/sort-colors.cpp
@@ -21,8 +21,11 @@ public:
 */
 
 
+
 // using duscth flag algorithm
-class Solution {
+// time complexity= o(n)
+// space = o(1)
+/*class Solution {
 public:
     void sortColors(vector<int>& a) {
         int n= a.size();
@@ -43,4 +46,34 @@ public:
     }
     }
 
+}; */
+
+
+// using count variable 
+class Solution{
+public:
+    void sortColors(vector<int>& a) {
+        int n= a.size();
+        int c0=0, c1=0, c2=0;
+        for(int i =0; i<n; i++){
+            if(a[i]==0){
+            c0++;
+        }
+            else if( a[i]==1){
+                c1++;
+            }
+            else{
+                c2++;
+            }
+        }
+        for(int i =0; i<c0; i++){
+            a[i]=0;
+        }
+        for(int i=c0 ; i< c0+c1; i++){
+            a[i]=1;
+        }
+        for(int i=c0+c1 ; i< n; i++){
+            a[i]=2;
+        }
+    } 
 };
