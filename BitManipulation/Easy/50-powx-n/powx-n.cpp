490bb48c2d231c40bc70441bfaf8d92b81d3b5a0
Shalini Nukella <106904788+shalininukella@users.noreply.github.com>
2023-09-23 15:04:20 +0530

    Time: 3 ms (40.55%) | Memory: 6.3 MB (12.32%) - LeetSync

---
+++ BitManipulation/Easy/50-powx-n/powx-n.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    double myPow(double a, int b) {
+        double ans = 1;
+        long sign;
+        if(b<0){
+           sign = long(b);
+    }
+        sign = abs(sign);
+        while(sign!=0){
+            if(sign&1){
+                ans = (ans * a);
+            }
+            a = (a * a);
+            sign = sign >> 1;
+        }
+        if( b > 0){
+        return ans;
+    }
+    else {
+        return 1/ans;
+    }
+    }
+};
+        
+        
