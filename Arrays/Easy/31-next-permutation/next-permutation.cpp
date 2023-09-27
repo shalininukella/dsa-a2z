9d69ce1b0d5de1c4703cb6e850a98654d28859d8
Shalini Nukella <106904788+shalininukella@users.noreply.github.com>
2023-09-27 12:31:27 +0530

    Time: 4 ms (47.14%) | Memory: 12.5 MB (8.19%) - LeetSync

--- Arrays/Easy/31-next-permutation/next-permutation.cpp
+++ Arrays/Easy/31-next-permutation/next-permutation.cpp
@@ -15,14 +15,46 @@
 //     }
 // };
 
+
+
 //using next_permutation funtion 
 //time complexity = O(N)
 //space complexity = O(1)
+// class Solution {
+// public:
+//     void nextPermutation(vector<int>& a) {
+//         next_permutation(a.begin(), a.end());
+//     }
+// };
+
+
+
 class Solution {
 public:
     void nextPermutation(vector<int>& a) {
-        next_permutation(a.begin(), a.end());
+        int n = a.size();
+        int i, brkpnt, greater;
+        for (i = n - 1; i > 0; i--) {
+            if (a[i] > a[i - 1]) {
+                brkpnt = i - 1;
+                break;
+            }
+        }
+
+        if (i <= 0) {
+            // If no breakpoint is found, the array is in descending order.
+            // Reverse the entire array to get the smallest permutation.
+            reverse(a.begin(), a.end());
+        } else {
+            for (int j = n - 1; j >= i; j--) {
+                if (a[i - 1] < a[j]) {
+                    greater = j;
+                    break;
+                }
+            }
+            swap(a[brkpnt], a[greater]);
+            reverse(a.begin() + brkpnt + 1, a.end());
+        }
     }
 };
-        
 
