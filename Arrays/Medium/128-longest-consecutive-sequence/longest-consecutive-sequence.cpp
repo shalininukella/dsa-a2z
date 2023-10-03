ed4496552ca3e71bfed5888187479a8ea5a2b7f7
Shalini Nukella <106904788+shalininukella@users.noreply.github.com>
2023-10-04 03:39:40 +0530

    Time: 132 ms (55.09%) | Memory: 70.9 MB (9.64%) - LeetSync

--- Arrays/Medium/128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ Arrays/Medium/128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,42 +1,42 @@
-// //optimal (striver)
-// //tc=o(2n)+o(n)=o(n)
-// //sc=o(n)
-// class Solution {
-// public:
-//     int longestConsecutive(vector<int>& a) {
-//         if(a.size()==0){
-//             return 0;
-//         }
-//         int longest = 1; 
-//         int cnt = 1;
-//         unordered_set<int> st;
-//         //to add elements into the set (unique will be added)
-//         for(int i =0;i < a.size();i++){
-//             st.insert(a[i]);
-//         }       
+//optimal (striver)
+//tc=o(2n)+o(n)=o(n)
+//sc=o(n)
+class Solution {
+public:
+    int longestConsecutive(vector<int>& a) {
+        if(a.size()==0){
+            return 0;
+        }
+        int longest = 1; 
+        int cnt = 1;
+        unordered_set<int> st;
+        //to add elements into the set (unique will be added)
+        for(int i =0;i < a.size();i++){
+            st.insert(a[i]);
+        }       
 
-//         //to iterate thtrough every element, 
-//         for(auto it: st){
+        //to iterate thtrough every element, 
+        for(auto it: st){
 
-//             //continue the loop if the current element(it)-1 is found i.e the starting element of the sequence is not found.
-//             if(st.find(it-1)!=st.end()){
-//                 continue;
-//             }
+            //continue the loop if the current element(it)-1 is found i.e the starting element of the sequence is not found.
+            if(st.find(it-1)!=st.end()){
+                continue;
+            }
 
-//             //if currentelement-1 is no more there, the "it" is the starting of the sequence 
-//             else if(st.find(it-1)==st.end()){
-//                 int x=it;
-//                 while(st.find(x+1)!=st.end()){
-//                     cnt++;
-//                     x++;
-//                 }
-//                 longest= max(longest, cnt);
-//                 cnt=1;
-//             }
-//         } 
-//     return longest;
-//     }
-// };
+            //if currentelement-1 is no more there, the "it" is the starting of the sequence 
+            else if(st.find(it-1)==st.end()){
+                int x=it;
+                while(st.find(x+1)!=st.end()){
+                    cnt++;
+                    x++;
+                }
+                longest= max(longest, cnt);
+                cnt=1;
+            }
+        } 
+    return longest;
+    }
+};
 
 // //better approach using sorting(mine)
 // //tc=o(nlogn)+o(n)
@@ -67,36 +67,42 @@
 // };
 
 
-//better approach using sorting(striver)
-//tc=o(nlogn)+o(n)
-//sc=o(1)
-class Solution {
-public:
-    int longestConsecutive(vector<int>& a) {
-        int n = a.size();
-        if(n==0) return 0;
-         
-        int lastSmall=INT_MIN; 
-        int longest=1;
-        int cnt=0; //or cnt=1, lastSmall=a[0], for loop i=1 to n
+// // Brute force (striver)
+// //tc = o(n^2) TLE
+// //sc= o(1)
+// class Solution {
+// public:
 
-        sort(a.begin(), a.end());
+//     bool linearSearch(vector<int>& a, int num){
+//         int n = a.size();
+//         for(int i=0;i<n;i++){
+//             if(a[i]==num){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+//     int longestConsecutive(vector<int>& a) {
 
-        for(int i=0;i<n;i++){
-            if(lastSmall==a[i]-1){
-                cnt++;
-                lastSmall=a[i];
-            }
+//         int n = a.size();
+//         if(n==0) return 0;
 
-            //if lastSmall==a[i], then do ntg just continue the loop, hence if the lastSmall!=a[i], then the sequence ended there and hence cnt=1 and fresh start with lastSmall as the current element
-            else if (a[i]!=lastSmall){
-                cnt=1;
-                lastSmall=a[i];
-            }
-            longest=max(longest, cnt);
-        }
-        return longest;
-    }
-};
+//         int longest=1;
+//         int cnt=1;
+
+//         for(int i =0; i<n;i++){
+//             int x = a[i];
+//             while(linearSearch(a, x+1)){
+//                 cnt++;
+//                 x++;
+//             }
+//             longest=max(longest, cnt);
+//             cnt=1;
+//         }
+//         return longest;
+//     }
+// };
+
+
 
 
