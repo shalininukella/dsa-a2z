1e7aab3b91afbd53aa9c38974a0f38c8292a6ab9
Shalini Nukella <106904788+shalininukella@users.noreply.github.com>
2023-10-04 03:20:30 +0530

    Time: 66 ms (90.21%) | Memory: 46.2 MB (74.16%) - LeetSync

--- Arrays/Medium/128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ Arrays/Medium/128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,38 +1,67 @@
+// //optimal (striver)
+// //tc=o(2n)+o(n)=o(n)
+// //sc=o(n)
+// class Solution {
+// public:
+//     int longestConsecutive(vector<int>& a) {
+//         if(a.size()==0){
+//             return 0;
+//         }
+//         int longest = 1; 
+//         int cnt = 1;
+//         unordered_set<int> st;
+//         //to add elements into the set (unique will be added)
+//         for(int i =0;i < a.size();i++){
+//             st.insert(a[i]);
+//         }       
+
+//         //to iterate thtrough every element, 
+//         for(auto it: st){
+
+//             //continue the loop if the current element(it)-1 is found i.e the starting element of the sequence is not found.
+//             if(st.find(it-1)!=st.end()){
+//                 continue;
+//             }
+
+//             //if currentelement-1 is no more there, the "it" is the starting of the sequence 
+//             else if(st.find(it-1)==st.end()){
+//                 int x=it;
+//                 while(st.find(x+1)!=st.end()){
+//                     cnt++;
+//                     x++;
+//                 }
+//                 longest= max(longest, cnt);
+//                 cnt=1;
+//             }
+//         } 
+//     return longest;
+//     }
+// };
+
+//better approach using sorting
+//tc=o(nlogn)+o(n)
+//sc=o(1)
 class Solution {
 public:
     int longestConsecutive(vector<int>& a) {
         if(a.size()==0){
             return 0;
         }
-        int longest = 1; 
-        int cnt = 1;
-        unordered_set<int> st;
-        //to add elements into the set (unique will be added)
-        for(int i =0;i < a.size();i++){
-            st.insert(a[i]);
-        }       
-
-        //to iterate thtrough every element, 
-        for(auto it: st){
-
-            //continue the loop if the current element(it)-1 is found i.e the starting element of the sequence is not found.
-            if(st.find(it-1)!=st.end()){
+        int longest=1;
+        int cnt=1;
+        sort(a.begin(), a.end());
+        for(int i =0;i<a.size()-1;i++){
+            if(a[i+1]-a[i]==1){
+                cnt++;
+            }
+            else if (a[i+1]-a[i]==0){
                 continue;
             }
-
-            //if currentelement-1 is no more there, the "it" is the starting of the sequence 
-            else if(st.find(it-1)==st.end()){
-                int x=it;
-                while(st.find(x+1)!=st.end()){
-                    cnt++;
-                    x++;
-                }
-                longest= max(longest, cnt);
+            else if(a[i+1]-a[i]!=1){
                 cnt=1;
             }
-        } 
-    return longest;
+            longest=max(longest, cnt);
+        }
+        return longest;
     }
 };
-
-
