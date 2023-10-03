e5938c729d81052fad8203c26fb80d2e21ea9f9a
Shalini Nukella <106904788+shalininukella@users.noreply.github.com>
2023-10-04 03:10:05 +0530

    Time: 128 ms (58.92%) | Memory: 70.8 MB (12.02%) - LeetSync

---
+++ Arrays/Medium/128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+//     int longestConsecutive(vector<int>& a) {
+//         if(a.size()==0){
+//             return 0;
+//         }
+//         int longest = 1; 
+//         int cnt = 1;
+//         unordered_set<int> st;
+//         //to add elements into the set (unique will be added)
+//         for(int i =0;i < a.size();i++){
+//             st.insert(a[i]);
+//         }       
+
+//         //to iterate thtrough every element, 
+//         for(auto it: st){
+
+//             //continue the loop if the current element(it)-1 is found i.e the starting element of the sequence is not found.
+//             if(st.find(it-1)!=st.end()){
+//                 continue;
+//             }
+
+//             //if currentelement-1 is no more there, the "it" is the starting of the sequence 
+//             else if(st.find(it-1)==st.end()){
+//                 int x=it;
+//                 while(st.find(x+1)!=st.end()){
+//                     cnt++;
+//                     x++;
+//                 }
+//                 longest= max(longest, cnt);
+//             }
+//         } 
+//     return longest;
+//     }
+//};
+
+
+int longestConsecutive(vector<int>&a) {
+    int n = a.size();
+    if (n == 0) return 0;
+
+    int longest = 1;
+    unordered_set<int> st;
+    //put all the array elements into set:
+    for (int i = 0; i < n; i++) {
+        st.insert(a[i]);
+    }
+
+    //Find the longest sequence:
+    for (auto it : st) {
+        //if 'it' is a starting number:
+        if (st.find(it - 1) == st.end()) {
+            //find consecutive numbers:
+            int cnt = 1;
+            int x = it;
+            while (st.find(x + 1) != st.end()) {
+                x = x + 1;
+                cnt = cnt + 1;
+            }
+            longest = max(longest, cnt);
+        }
+    }
+    return longest;
+
+}
+};
