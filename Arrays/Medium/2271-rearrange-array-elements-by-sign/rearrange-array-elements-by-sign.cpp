0f05c8e212a67c613af0f5353ca4a9477f9b7aee
Shalini Nukella <106904788+shalininukella@users.noreply.github.com>
2024-06-05 05:20:02 +0530

    Time: 139 ms (69.97%) | Memory: 126.2 MB (85.59%) - LeetSync

--- Arrays/Medium/2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ Arrays/Medium/2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -1,18 +1,40 @@
+// class Solution {
+// public:
+//     vector<int> rearrangeArray(vector<int>& a) {
+//         vector<int> pos;
+//         vector<int> neg;
+//         for(int i=0;i<a.size();i++){
+//             if(a[i]>0)
+//                 pos.push_back(a[i]);
+//             else 
+//                 neg.push_back(a[i]);
+//         }
+//         for(int i=0;i<a.size()/2;i++){
+//             a[2*i] = pos[i];
+//             a[(2*i)+1] = neg[i];
+//          }
+//          return a;
+//     }
+// };
+
+
+
 class Solution {
-public:
-    vector<int> rearrangeArray(vector<int>& a) {
-        vector<int> pos;
-        vector<int> neg;
-        for(int i=0;i<a.size();i++){
-            if(a[i]>0)
-                pos.push_back(a[i]);
-            else 
-                neg.push_back(a[i]);
+    public:
+        vector<int> rearrangeArray(vector<int>& a) {
+            int n = a.size(); // Initialize n with the size of the input vector
+            int posInd = 0, negInd = 1;
+            vector<int> ans(n, 0); // Use n to initialize the size of ans
+
+            for (int i = 0; i < a.size(); i++) {
+                if (a[i] > 0) {
+                    ans[posInd] = a[i];
+                    posInd += 2;
+                } else {
+                    ans[negInd] = a[i];
+                    negInd += 2;
+                }
+            }
+            return ans;
         }
-        for(int i=0;i<a.size()/2;i++){
-            a[2*i] = pos[i];
-            a[(2*i)+1] = neg[i];
-         }
-         return a;
-    }
 };
