4f5c88098dfd0911acb7895bd481f7792f6c955a
Shalini Nukella <106904788+shalininukella@users.noreply.github.com>
2024-06-05 05:12:24 +0530

    Time: 148 ms (38.70%) | Memory: 137 MB (24.95%) - LeetSync

---
+++ Arrays/Medium/2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& a) {
+        vector<int> pos;
+        vector<int> neg;
+
+        for(int i=0;i<a.size();i++){
+            if(a[i]>0)
+                pos.push_back(a[i]);
+            else 
+                neg.push_back(a[i]);
+        }
+        for(int i=0;i<a.size()/2;i++){
+            a[2*i] = pos[i];
+            a[(2*i)+1] = neg[i];
+         }
+         return a;
+    }
+};
