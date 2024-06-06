1f09a9585889bd1aad410eeca757b69d5484226f
Shalini Nukella <106904788+shalininukella@users.noreply.github.com>
2024-06-06 23:31:55 +0530

    Time: 10 ms (49.08%) | Memory: 18.4 MB (45.01%) - LeetSync

--- Arrays/Hard/229-majority-element-ii/majority-element-ii.cpp
+++ Arrays/Hard/229-majority-element-ii/majority-element-ii.cpp
@@ -1,3 +1,6 @@
+//1)striver optimal 
+//TC = O(n) + O(n) = O(n), sc=o(1)
+
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& a) {
@@ -50,5 +53,69 @@ public:
         return res;
     }
 };
+
+
+//2) better - using maps
+//tc = O(N), sc = o(n)
+
+// vector<int> majorityElement(vector<int> v) {
+//     int n = v.size(); //size of the array
+//     vector<int> ls; // list of answers
+
+//     //declaring a map:
+//     map<int, int> mpp;
+
+//     // least occurrence of the majority element:
+//     int mini = int(n / 3) + 1;
+
+//     //storing the elements with its occurnce:
+//     for (int i = 0; i < n; i++) {
+//         mpp[v[i]]++;
+
+//         //checking if v[i] is
+//         // the majority element:
+//         if (mpp[v[i]] == mini) {
+//             ls.push_back(v[i]);
+//         }
+//         if (ls.size() == 2) break;
+//     }
+
+//     return ls;
+// }
+
+
+
+//3) brute force 
+//tc = O(N^2), sc = o(1)
+
+// vector<int> majorityElement(vector<int> v) {
+//     int n = v.size(); //size of the array
+//     vector<int> ls; // list of answers
+
+//     for (int i = 0; i < n; i++) {
+//         //selected element is v[i]:
+//         // Checking if v[i] is not already
+//         // a part of the answer:
+//         if (ls.size() == 0 || ls[0] != v[i]) {
+//             int cnt = 0;
+//             for (int j = 0; j < n; j++) {
+//                 // counting the frequency of v[i]
+//                 if (v[j] == v[i]) {
+//                     cnt++;
+//                 }
+//             }
+
+//             // check if frquency is greater than n/3:
+//             if (cnt > (n / 3))
+//                 ls.push_back(v[i]);
+//         }
+
+//         if (ls.size() == 2) break;
+//     }
+
+//     return ls;
+// }
+
+
 
 
