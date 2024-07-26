eb7a1958597b28710401fbb4c2b3d82cbdf67dbc
Shalini Nukella <106904788+shalininukella@users.noreply.github.com>
2024-07-27 00:39:16 +0530

    Update sliding-window-maximum.cpp

--- Arrays/Hard/239-sliding-window-maximum/sliding-window-maximum.cpp
+++ Arrays/Hard/239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,10 +1,26 @@
+
+
+
+
+
+
+
+//2)
+//OMG!!!! my solution - leetcode hard - sliding window - fixed size - might not be optimal
+
+//Time Complexity: 
+//𝑂(𝑛log 𝑘)
+
+//Space Complexity: 
+//O(k)
+//k = size of map
+
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int i=0,j=0;
         int n = nums.size();
         vector<int> ans;
-        //int maxi = INT_MIN;
         map<int, int> m;
         while(j < n){
             m[nums[j]]++;
