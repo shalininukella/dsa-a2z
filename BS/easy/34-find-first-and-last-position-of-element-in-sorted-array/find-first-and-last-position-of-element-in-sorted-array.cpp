af79ffd73419f3a5c45bdc211ae7cefc2e2ae99b
Shalini Nukella <106904788+shalininukella@users.noreply.github.com>
2024-08-03 19:09:30 +0530

    Time: 5 ms (62.69%) | Memory: 16.3 MB (18.99%) - LeetSync

--- BS/easy/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ BS/easy/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,35 +1,6 @@
-// class Solution {
-// public:
-//     vector<int> searchRange(vector<int>& nums, int target) {
-//         int left = 0, right = nums.size() - 1;
-//         int first = -1, last = -1;
-        
-//         while (left <= right) {
-//             int mid = left + (right - left) / 2;
-//             if (nums[mid] == target) {
-//                 first = mid;
-//                 last = mid;
-//                 while (first > 0 && nums[first - 1] == target) {
-//                     first--;
-//                 }
-//                 while (last < nums.size() - 1 && nums[last + 1] == target) {
-//                     last++;
-//                 }
-//                 break;
-//             }
-//             else if (nums[mid] < target) {
-//                 left = mid + 1;
-//             }
-//             else {
-//                 right = mid - 1;
-//             }
-//         }
-        
-//         return {first, last};
-//     }
-// };
-
-
+//optimal 
+// tc : O(logn)
+//space : 0(1)
 class Solution {
 public:
     vector<int> searchRange(vector<int>& a, int target) {
@@ -37,25 +8,65 @@ public:
         int l=0,h=a.size()-1;
         int first =-1, last =-1;
 
+//to ge the first occurence
         while(l<=h){
             int m = l + (h - l) / 2;
-            if(a[m]==target){
-                first =m; last = m;
-                while(first>0 && target==a[first-1]){
-                    first --;
-                }
-
-                while(last<a.size()-1 && a[last+1]==target){
-                    last++;
-                }
-                break;
-            }
-
-            else if (a[m]>target){
+            if(target<=a[m]){
+                if (a[m] == target) 
+                    first = m;
                 h=m-1;
             }
-            else l=m+1;
+            else {
+                l=m+1;
+            }
         }
-        return {first,last};
+//to get the last occurence 
+        l=0,h=a.size()-1;
+        while(l<=h){
+            int m = l + (h - l) / 2;
+            if(a[m]<=target){
+                if (a[m] == target) 
+                    last = m;
+                l=m+1;
+            }
+            else{
+                h=m-1;
+            }
         }
+                
+    return {first,last};
+    }
 };
+
+// // tc : O(logn)+O(k) = 0(n)
+// //space : 0(1)
+
+// class Solution {
+// public:
+//     vector<int> searchRange(vector<int>& a, int target) {
+//         vector<int> ans;
+//         int l=0,h=a.size()-1;
+//         int first =-1, last =-1;
+
+//         while(l<=h){
+//             int m = l + (h - l) / 2;
+//             if(a[m]==target){
+//                 first =m; last = m;
+//                 while(first>0 && target==a[first-1]){
+//                     first --;
+//                 }
+
+//                 while(last<a.size()-1 && a[last+1]==target){
+//                     last++;
+//                 }
+//                 break;
+//             }
+
+//             else if (a[m]>target){
+//                 h=m-1;
+//             }
+//             else l=m+1;
+//         }
+//         return {first,last};
+//         }
+// };
