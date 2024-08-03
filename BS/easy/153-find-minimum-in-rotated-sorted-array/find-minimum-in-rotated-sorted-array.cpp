1ed3181ffb3c1ae3f729295a2db412ae425a4ee7
Shalini Nukella <106904788+shalininukella@users.noreply.github.com>
2024-08-03 19:54:35 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (18.60%) - LeetSync

---
+++ BS/easy/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -0,0 +1,69 @@
+// class Solution {
+// public:
+//     int findMin(vector<int>& nums) {
+//         int ans = nums[0];
+//         int low = 0 , high = nums.size()-1;
+//         if(nums[low] < nums[high]){
+//             return nums[low];
+//         }
+//         while(low <= high){
+//             if(nums[low] < nums[high]){
+//                 ans = min(ans , nums[low]);
+//                 break;
+//             }
+
+//             int mid = (low + high)/2;
+//             ans = min(ans , nums[mid]);
+
+//             if(nums[mid] >= nums[low]){
+//                 low = mid + 1;
+//             }
+//             else{
+//                 high = mid -1 ;
+//             }
+            
+//         }
+
+//         return ans;
+        
+//     }
+// };
+
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findMin(vector<int>& a) {
+        int l = 0, h = a.size() - 1;
+
+        // Handle the case where the array is not rotated
+        if (a[l] <= a[h]) {
+            return a[l];
+        }
+
+        while (l <= h) {
+            int m = (l + h) / 2;
+
+            // Check if mid element is the minimum
+            if (m > 0 && a[m] < a[m - 1]) {
+                return a[m];
+            }
+            // Check if mid+1 element is the minimum
+            if (m < a.size() - 1 && a[m] > a[m + 1]) {
+                return a[m + 1];
+            }
+
+            // Adjust search range
+            if (a[m] >= a[l]) {
+                l = m + 1;
+            } else {
+                h = m - 1;
+            }
+        }
+
+        // This line should never be reached
+        return -1;
+    }
+};
