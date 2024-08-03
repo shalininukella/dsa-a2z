cd94dcff1d6bb818479c9b3e729b7f5eb4898a31
Shalini Nukella <106904788+shalininukella@users.noreply.github.com>
2024-08-03 18:01:08 +0530

    Create floor_or_lowerbound.cpp

---
+++ BS/easy/floor_or_lowerbound.cpp
@@ -0,0 +1,38 @@
+//https://www.geeksforgeeks.org/problems/floor-in-a-sorted-array-1587115620/1?track=DSASP-Searching&amp%253BbatchId=154&utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=floor-in-a-sorted-array#
+
+/*
+Time Complexity: O(logN), where N = size of the given array.
+Space Complexity: O(1) 
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int lowerBound(vector<int> arr, int n, int x) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] >= x) {
+            ans = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr = {3, 5, 8, 15, 19};
+    int n = 5, x = 9;
+    int ind = lowerBound(arr, n, x);
+    cout << "The lower bound is the index: " << ind << "\n";
+    return 0;
+}
+
+
