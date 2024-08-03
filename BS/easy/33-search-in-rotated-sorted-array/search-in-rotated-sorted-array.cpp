9c613fc05446f1ad23d88d0492522b7fd13dc4a5
Shalini Nukella <106904788+shalininukella@users.noreply.github.com>
2024-08-03 19:27:07 +0530

    Time: 3 ms (59.25%) | Memory: 14 MB (13.45%) - LeetSync

--- BS/easy/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ BS/easy/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,29 +1,65 @@
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//         int left = 0;
+//         int right = nums.size() - 1;
+
+//         while (left <= right) {
+//             int mid = (left + right) / 2;
+
+//             if (nums[mid] == target) {
+//                 return mid;
+//             } else if (nums[mid] >= nums[left]) {
+//                 if (nums[left] <= target && target <= nums[mid]) {
+//                     right = mid - 1;
+//                 } else {
+//                     left = mid + 1;
+//                 }
+//             } else {
+//                 if (nums[mid] <= target && target <= nums[right]) {
+//                     left = mid + 1;
+//                 } else {
+//                     right = mid - 1;
+//                 }
+//             }
+//         }
+
+//         return -1;        
+//     }
+// };
+
+
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size() - 1;
+    int search(vector<int>& a, int target) {
+        int l=0,h=a.size()-1;
+        while(l<=h){
+            int m=(l+h)/2;
 
-        while (left <= right) {
-            int mid = (left + right) / 2;
+            if(a[m]==target){
+                return m;
+            }
 
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] >= nums[left]) {
-                if (nums[left] <= target && target <= nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
+            else if(a[l]<=a[m]){
+                //left half is sorted 
+                if(target >= a[l] && target <= a[m]){
+                    h=m-1;
                 }
-            } else {
-                if (nums[mid] <= target && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
+                else{
+                    l=m+1;
                 }
             }
-        }
 
-        return -1;        
+            else {
+                //right is sorted
+                if(target>=a[m] && target<=a[h]){
+                    l=m+1;
+                }
+                else{
+                    h=m-1;
+                }
+            } 
+        } 
+        return -1;  // if not found 
     }
 };
