146467e0a50f52efa274906c8fc1025765053677
Shalini Nukella <106904788+shalininukella@users.noreply.github.com>
2024-08-03 18:02:41 +0530

    Create upperbound.cpp

---
+++ BS/easy/upperbound.cpp
@@ -0,0 +1,39 @@
+//https://www.geeksforgeeks.org/problems/ceil-the-floor2802/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=ceil-the-floor
+
+/* 
+Time Complexity: O(logN), where N = size of the given array.
+Reason: We are basically using the Binary Search algorithm.
+
+Space Complexity: O(1) as we are using no extra space.
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int upperBound(vector<int> &arr, int x, int n) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] > x) {
+            ans = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr = {3, 5, 8, 9, 15, 19};
+    int n = 6, x = 9;
+    int ind = upperBound(arr, x, n);
+    cout << "The upper bound is the index: " << ind << "\n";
+    return 0;
+}
+
