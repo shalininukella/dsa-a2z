93d4724f8179c8aa0f99b66a6d0a27168a86ab57
Shalini Nukella <106904788+shalininukella@users.noreply.github.com>
2024-08-03 18:23:07 +0530

    Time: 4 ms (36.54%) | Memory: 12.2 MB (39.85%) - LeetSync

--- BS/easy/35-search-insert-position/search-insert-position.cpp
+++ BS/easy/35-search-insert-position/search-insert-position.cpp
@@ -1,48 +1,48 @@
-// class Solution {
-// public:
-//     int searchInsert(vector<int>& nums, int target) {
-//         int low=0;
-//         int high=nums.size();
-//         int mid;
-//         if(target>nums[high-1]){
-//             return high;
-//         }
-//         while(low<=high){
-//               mid=(low+high)/2;
-//             if(nums[mid]==target){  
-//                 return mid;
-//             }
-          
-//             if(target<nums[mid]){     
-//             high=mid-1;    
-//             }else{
-//             low=mid+1;        
-//             }
-          
-//         }
-//          return  low;   
-//     }
-// };
-
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         int low=0;
-        int high=nums.size()-1;
+        int high=nums.size();
         int mid;
-        int ans=-1;
-        if(target>nums[high]){
-            return high+1;
+        if(target>nums[high-1]){
+            return high;
         }
         while(low<=high){
               mid=(low+high)/2;
-            if(target<=nums[mid]){  
-                ans = mid;
-                high=mid-1;    
-            }else{
-                low=mid+1;        
+            if(nums[mid]==target){  
+                return mid;
             }
+          
+            if(target<nums[mid]){     
+            high=mid-1;    
+            }else{
+            low=mid+1;        
+            }
+          
         }
-         return  ans;   
+         return  low;   
     }
 };
+
+// class Solution {
+// public:
+//     int searchInsert(vector<int>& nums, int target) {
+//         int low=0;
+//         int high=nums.size()-1;
+//         int mid;
+//         int ans=-1;
+//         if(target>nums[high]){
+//             return high+1;
+//         }
+//         while(low<=high){
+//               mid=(low+high)/2;
+//             if(target<=nums[mid]){  
+//                 ans = mid;
+//                 high=mid-1;    
+//             }else{
+//                 low=mid+1;        
+//             }
+//         }
+//          return  ans;   
+//     }
+// };
